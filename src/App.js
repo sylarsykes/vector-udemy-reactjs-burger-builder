4a8dcc8c5021e55ccac4f975f597835e55b4d274
Juan Gonzalez Fernandez <juan.gonzalez.fernandez.jgf@outlook.com>
2020-05-24 19:59:45 +0200

    Create practice 2

--- src/App.js
+++ src/App.js
@@ -2,33 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { ExampleUserInput, ExampleUserOutput } from './components/practice1';
+import { ExampleChar, ExampleValidation } from './components/practice2';
 
 class App extends Component {
   state = {
-    username: 'supermax'
-  };
+    userInput: '',
+  }
 
-  usernameChangeHandler = event => this.setState({username: event.target.value})
+  inputChangeHandler = event => this.setState({userInput: event.target.value})
+
+  deleteCharHandler = index => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
+  }
 
   render() {
+    const charList = this.state.userInput.split('').map((ch, index) => 
+      <ExampleChar character={ch} key={index} clicked={() => this.deleteCharHandler(index)} />);
+
     return (
       <div className="App">
-        <ol>
-          <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
-          <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
-          <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
-          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
-          <li>Add a method to manipulate the state (=> an event-handler method)</li>
-          <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
-          <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
-          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
-          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
-        </ol>
-        <ExampleUserInput type={'text'} className={'example-user-input'} style={ {boder: '2px solid red'} } changed={this.usernameChangeHandler} currentName={this.state.username} />
-        <ExampleUserOutput username={this.state.username}>I hope I'll be overwritten</ExampleUserOutput>
-        <ExampleUserOutput username={this.state.username} />
-        <ExampleUserOutput username="Max" />
+        <ExampleUserInput type={'text'} className={'example-user-input'} style={ {boder: '2px solid red'} } changed={this.inputChangeHandler} currentName={this.state.userInput} />
+        <ExampleUserOutput username={this.state.userInput} />
+        <ExampleValidation inputLength={this.state.userInput.length} />
+        {charList}
       </div>
     );
   }
